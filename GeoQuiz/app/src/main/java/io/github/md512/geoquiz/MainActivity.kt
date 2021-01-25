13732ee56d166ccb1e18640d6332a776f3c3f204
Dmitry Mokroborodov <dimok179@yandex.ru>
2021-01-25 23:03:52 +0300

    add UI elements

--- GeoQuiz/app/src/main/java/io/github/md512/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/io/github/md512/geoquiz/MainActivity.kt
@@ -2,10 +2,17 @@ package io.github.md512.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
+private lateinit var trueButton: Button
+private lateinit var falseButton: Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
     }
 }
