35c980464ceb38fbeb86b91836f0108453503efe
Dmitry Mokroborodov <dimok179@yandex.ru>
2021-02-03 07:04:02 +0300

    questions

--- GeoQuiz/app/src/main/java/io/github/md512/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/io/github/md512/geoquiz/MainActivity.kt
@@ -5,10 +5,22 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 private lateinit var trueButton: Button
 private lateinit var falseButton: Button
+private lateinit var nextButton: Button
+private lateinit var questionTextView: TextView
+
+private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true))
+private var currentIndex = 0
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +29,13 @@ class MainActivity : AppCompatActivity() {
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
+
+        nextButton.setOnClickListener{view: View ->
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
 
         trueButton.setOnClickListener{view: View ->
             val toast = Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT)
@@ -27,5 +46,16 @@ class MainActivity : AppCompatActivity() {
         falseButton.setOnClickListener{view: View ->
             Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
         }
+
+        updateQuestion()
+    }
+
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAndwer(userAnswer: Boolean) {
+        
     }
 }
