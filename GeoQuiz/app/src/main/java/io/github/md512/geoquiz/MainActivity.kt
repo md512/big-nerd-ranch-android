8f6ceec70fe76a34cad5b0da63815cd34b720a87
Dmitry Mokroborodov <dimok179@yandex.ru>
2021-03-27 09:35:52 +0300

    add animation

--- GeoQuiz/app/src/main/java/io/github/md512/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/io/github/md512/geoquiz/MainActivity.kt
@@ -1,7 +1,9 @@
 package io.github.md512.geoquiz
 
+import android.app.ActivityOptions
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
@@ -62,10 +64,17 @@ class MainActivity : AppCompatActivity() {
             updateQuestion()
         }
 
-        cheatButton.setOnClickListener{ view: View ->
+        cheatButton.setOnClickListener{ view ->
             val answerIsTrue = quizViewModel.currentQuestionAnswer
             val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
-            startActivityForResult(intent, REQUEST_CODE_CHEAT)
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                val options =
+                    ActivityOptions.makeClipRevealAnimation(view, 0, 0, view.width, view.height)
+                startActivityForResult(intent, REQUEST_CODE_CHEAT, options.toBundle())
+            } else {
+                startActivityForResult(intent, REQUEST_CODE_CHEAT)
+            }
         }
 
         updateQuestion()
